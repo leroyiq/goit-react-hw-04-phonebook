cc1e2e7f31dbed08c1ea5133b63fffbed3a459fe
leroyiq <leroyiq6@gmail.com>
2023-04-04 00:31:53 +0300

    Remade to hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import {
   Form,
   Button,
@@ -10,36 +10,34 @@ import { FcPlus } from 'react-icons/fc';
 import toast from 'react-hot-toast';
 import PropTypes from 'prop-types';
 
-const CLEAR_STATE = {
-  name: '',
-  number: '',
-};
+export const ContactForm = ({ addContact, onContacts }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-export class ContactForm extends Component {
-  static propTypes = {
-    addContact: PropTypes.func.isRequired,
-    onContacts: PropTypes.array.isRequired,
-  };
-
-  state = CLEAR_STATE;
-
-  handleChange = event => {
+  const handleChange = event => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'number':
+        return setNumber(value);
+      default:
+        throw new Error(`Unsupported type of ${name}`);
+    }
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    const { name, number } = this.state;
-    const { onContacts } = this.props;
+
     const checkContact = onContacts.some(
       contact =>
         contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
     );
 
     if (!checkContact) {
-      this.props.addContact({ name, number });
-      this.setState(CLEAR_STATE);
+      addContact({ name, number });
+      setName('');
+      setNumber('');
       return;
     }
     toast.error(`${name} is already in contact`, {
@@ -47,43 +45,45 @@ export class ContactForm extends Component {
     });
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <div>
-        <Form onSubmit={this.handleSubmit} aria-controls="form">
-          <Label>
-            Name
-            <Input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              placeholder="David Sterling"
-              //     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              //   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </Label>
+  return (
+    <div>
+      <Form onSubmit={handleSubmit} aria-controls="form">
+        <Label>
+          Name
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            placeholder="David Sterling"
+            //     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            //   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
 
-          <Label>
-            Number
-            <Input
-              type="tel"
-              name="number"
-              value={number}
-              onChange={this.handleChange}
-              placeholder="+ 380 66 666 99 99"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </Label>
-          <Button type="submit">
-            <FcPlus size="20px" /> <Span> Add contact</Span>
-          </Button>
-        </Form>
-      </div>
-    );
-  }
-}
+        <Label>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleChange}
+            placeholder="+ 380 66 666 99 99"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+        <Button type="submit">
+          <FcPlus size="20px" /> <Span> Add contact</Span>
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+ContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+  onContacts: PropTypes.array.isRequired,
+};
